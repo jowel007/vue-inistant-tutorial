36991c0d7cda71d6bbc3a4b478867b099bcf3e49
jowel007 <abuhanifjowel1@gmail.com>
2024-03-18 13:08:29 +0600

    create component & inside parent componet call child component

---
+++ src/views/ParentView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>This is Parent Component</h2>
+        <ChildComponent/>
+    </div>
+</template>
+
+
+<script>
+
+import ChildComponent from '../components/Child.vue'
+
+export default {
+    name:'ParentComponent',
+    components:{
+        ChildComponent
+    }
+}
+
+</script>
